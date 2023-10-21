c074eaebdf4c0555bb0c6e467ef90849feca56f0
jellomanforever <julianmaleur123@gmail.com>
2023-10-21 16:42:26 -0400

    Add Terraform Block

---
+++ aws/Oct-21-2023/terraform-manifests/c1-versions.tf
@@ -0,0 +1,15 @@
+#Terraform Block
+terraform {
+  required_version = "~> 1.6.1"
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.22.0"
+    }
+  }
+}
+
+
+//Provider Block
+
+

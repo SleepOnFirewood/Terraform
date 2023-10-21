e7d596a71e18c104bf9b474a2b16c68e6bdac1f0
jellomanforever <julianmaleur123@gmail.com>
2023-10-21 16:57:56 -0400

    add tags

--- aws/Oct-21-2023/terraform-manifests/c2-ec2instance.tf
+++ aws/Oct-21-2023/terraform-manifests/c2-ec2instance.tf
@@ -1,4 +1,8 @@
 resource "aws_instance" "myec2vm" {
     ami = "ami-0df435f331839b2d6"
-    instance_type = "t3.micro"    
+    instance_type = "t3.micro"
+    user_data = file("${path.module}/app1-install.sh")
+    tags = {
+      "Name" = "Ec2"
+    }
 }

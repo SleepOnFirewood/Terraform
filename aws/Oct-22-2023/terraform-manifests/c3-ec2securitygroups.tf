dacfcb7397a55f004568d92576d822f174d10a81
jellomanforever <julianmaleur123@gmail.com>
2023-10-22 15:04:52 -0400

    Add web sg

--- aws/Oct-22-2023/terraform-manifests/c3-ec2securitygroups.tf
+++ aws/Oct-22-2023/terraform-manifests/c3-ec2securitygroups.tf
@@ -24,3 +24,38 @@ resource "aws_security_group" "vpc-ssh" {
     Name = "vpc-ssh"
   }
 }
+
+#Create Security Group - Web Traffic
+
+resource "aws_security_group" "vpc-web" {
+  name        = "vpc-web"
+  description = "Dev VPC web"
+
+  ingress {
+    description      = "allow port 80"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+    ingress {
+    description      = "allow port 443"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "vpc-web"
+  }
+}

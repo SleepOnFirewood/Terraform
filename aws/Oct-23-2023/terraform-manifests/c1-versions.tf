97198783416b4160908fb65875aaf9be43fed74d
jellomanforever <julianmaleur123@gmail.com>
2023-10-23 22:47:26 -0400

    Add c1-versions.tf

---
+++ aws/Oct-23-2023/terraform-manifests/c1-versions.tf
@@ -0,0 +1,17 @@
+#Terraform block
+terraform {
+  required_version = "~> 1.6.1"
+  required_providers {
+    aws = {
+        source= "hashicorp/aws"
+        version = "5.22.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+#Provider block

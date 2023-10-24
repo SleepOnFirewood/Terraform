99f8e88d6b842a4dd406bc29091754dcd1ec0269
jellomanforever <julianmaleur123@gmail.com>
2023-10-23 23:10:15 -0400

    edit ec2instance.tf to include id of security groups

--- aws/Oct-23-2023/terraform-manifests/c5-ec2instance.tf
+++ aws/Oct-23-2023/terraform-manifests/c5-ec2instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "myec2vm" {
   ami = data.aws_ami.amzlinux2.id
   instance_type = var.instance_type
   key_name = var.instance_keypair
-  vpc_security_group_ids = [aws_security_group.vpc-ssh, aws_security_group.vpc-web]
+  vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
     "Name"= "EC2 Demo"
   }

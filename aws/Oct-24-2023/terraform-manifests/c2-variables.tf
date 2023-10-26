87aef1b2dc9e0f8c48fde23042cd646d7bd20f0d
jellomanforever <julianmaleur123@gmail.com>
2023-10-25 21:37:15 -0400

    c2-variables.tf changes

--- aws/Oct-24-2023/terraform-manifests/c2-variables.tf
+++ aws/Oct-24-2023/terraform-manifests/c2-variables.tf
@@ -16,3 +16,20 @@ variable "instance_keypair" {
   description = "AWS EC2 keypair that needs to be associated with EC2 instance"
   default= "terraform-key"
 }
+
+# AWS EC2 instance type - map
+variable "instance_type_list" {
+  description = "EC2 instance type list "
+  type = list(string)
+  default = ["t3.micro", "t3.small"]
+}
+
+variable "instance_type_map" {
+  description = "EC2 instance type list "
+  type = map(string)
+  default = {
+    "dev" = "t3.micro"
+    "qa" = "t3.small"
+    "prod" = "t3.large"
+  }
+}
